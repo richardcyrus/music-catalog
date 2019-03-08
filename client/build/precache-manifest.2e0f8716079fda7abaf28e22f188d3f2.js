747f3948e86eac1b916dc9ab0f65add652598bb7
alexisrgrs <alexisrgrs@gmail.com>
2019-03-07 21:07:29 -0500

    Added Logo

---
+++ client/build/precache-manifest.2e0f8716079fda7abaf28e22f188d3f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "090d46b5b975b9b71179",
+    "url": "/static/js/main.090d46b5.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "090d46b5b975b9b71179",
+    "url": "/static/css/main.5328b27c.chunk.css"
+  },
+  {
+    "revision": "3171cd0fd4a1328b078133d77de51943",
+    "url": "/index.html"
+  }
+];
